ceb3b7025dc2b0a0761191fa0d0a2170554fc215
bernardtran99 <bernardtran99@gmail.com>
2022-05-01 22:31:08 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 import java.io.*;
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("helloworld...");
-	// test
-    }
-}
 
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("helloworld...");
+	}
+}
